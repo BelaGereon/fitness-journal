cc8edbdc24ec222f0fec9443090c68c90c472988
Bela Géréon <belagereon@gmx.de>
2024-12-30 15:07:33 +0100

    :memo: add a first test entry from diary

--- fitness-journal/src/App.tsx
+++ fitness-journal/src/App.tsx
@@ -4,32 +4,49 @@ function App() {
   return (
     <>
       <body>
-        <div className="content">
-          <h1>Hello</h1>
-          <p className="popout">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
-            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
-            sit quia, ducimus optio pariatur quos minus iusto commodi minima
-            vel.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
-            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
-            sit quia, ducimus optio pariatur quos minus iusto commodi minima
-            vel.
-          </p>
-          <p className="full">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
-            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
-            sit quia, ducimus optio pariatur quos minus iusto commodi minima
-            vel.
-          </p>
-          <p className="feature">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
-            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
-            sit quia, ducimus optio pariatur quos minus iusto commodi minima
-            vel.
-          </p>
+        <div className="diary-entry">
+          <h3 className="date">07th Dec 2024</h3>
+          <div className="content">
+            <div>
+              First week of maintenance is basically done, went ok so far. Got
+              sick and had to pause training again, and I was snacking a lot
+              lately because I didn't feel like cooking... Will work on that
+              next week. <br />
+              Average kcals/day are at ±2350 kcal for the week, overate a bit on
+              some days.
+            </div>
+            <div className="popout">
+              Average weight went <b>DOWN</b> by ±0,5kg over the week, so I
+              should definitely start to increase my kcals and see where it goes
+              from there.
+            </div>
+            <div>
+              If I go by the formulas, I should increase kcals to about
+              2700kcals/day... Feels a bit much to be honest, but that would
+              also fit the weightloss-rate of ±1lbs/week... <br />
+              I'll set my goal to <b>2700kcals</b> and then see what my weight
+              does the next week. <br />
+              Hopefully with weight-training starting again on wednesday, I
+              won't gain that much weight and just maintain my weight, maybe
+              even recomp a little...
+            </div>
+            <div className="popout">
+              I'll stay at 2700kcals at least on week, maybe even two to make
+              sure I account for water-weight-fluctuations in case my weight
+              shoots up the next week.
+            </div>
+            <div className="feature">
+              <b>TAKEAWAY FOR NEXT DIET:</b>
+              <br />
+              I think it's a good idea for me to incorporate diet-breaks into
+              the next fatloss-phase, maybe at a 3:1-ratio (3 weeks fatloss, 1
+              week maintenance) and use the maintenance-weeks or -days for
+              deloading as well.
+              <br />
+              That way I can hopefully offset metaboloc adaptations and also get
+              a little periodization for my training going.
+            </div>
+          </div>
         </div>
       </body>
     </>
