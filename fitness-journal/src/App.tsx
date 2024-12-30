5a0cefabc6309a8fbb072bc49f555d51a144bdf7
Bela Géréon <belagereon@gmx.de>
2024-12-30 13:42:06 +0100

    :sparkles: add grid to app

--- fitness-journal/src/App.tsx
+++ fitness-journal/src/App.tsx
@@ -4,12 +4,33 @@ function App() {
   return (
     <>
       <body>
-        <h1>Hello</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
-          quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus sit
-          quia, ducimus optio pariatur quos minus iusto commodi minima vel.
-        </p>
+        <div className="content">
+          <h1>Hello</h1>
+          <p className="popout">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
+            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
+            sit quia, ducimus optio pariatur quos minus iusto commodi minima
+            vel.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
+            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
+            sit quia, ducimus optio pariatur quos minus iusto commodi minima
+            vel.
+          </p>
+          <p className="full">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
+            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
+            sit quia, ducimus optio pariatur quos minus iusto commodi minima
+            vel.
+          </p>
+          <p className="feature">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, id
+            quam! Nobis qui deserunt assumenda aliquam id nulla necessitatibus
+            sit quia, ducimus optio pariatur quos minus iusto commodi minima
+            vel.
+          </p>
+        </div>
       </body>
     </>
   );
