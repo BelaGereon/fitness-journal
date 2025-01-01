04fc75dcc4c6bed48d30ee72f1e9141fc24d957a
Bela Géréon <belagereon@gmx.de>
2025-01-01 17:03:30 +0100

    :recycle: refactor unit test

--- fitness-journal/src/components/diary-entry/DiaryEntry.test.tsx
+++ fitness-journal/src/components/diary-entry/DiaryEntry.test.tsx
@@ -4,8 +4,14 @@ import "@testing-library/jest-dom";
 import DiaryEntry from "./DiaryEntry";
 
 describe("DiaryEntry Component", () => {
+  const date = "01. Jan 2025";
+
   it("renders without crashing", () => {
-    render(<DiaryEntry date="01. Jan 2025"></DiaryEntry>);
+    render(
+      <DiaryEntry date={date}>
+        <div>Child content</div>
+      </DiaryEntry>
+    );
 
     expect(screen.getByText("01. Jan 2025")).toBeInTheDocument();
   });
