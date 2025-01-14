fb6792557f9b292abd7532ecddd2b4a88f0dbf81
Bela Géréon <belagereon@gmx.de>
2025-01-14 19:09:24 +0100

    :art: enhance DiaryEntry styles and wrap App component with ThemeProvider

--- fitness-journal/src/main.tsx
+++ fitness-journal/src/main.tsx
@@ -4,8 +4,12 @@ import "./index.css";
 import "../public/Colors.css";
 import App from "./App.tsx";
 
+import { ThemeProvider } from "@material-tailwind/react";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
